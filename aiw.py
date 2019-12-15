91476045f3676e278c20f03accb771ca84cdaa67
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2019-12-15 12:06:18 +0100

    Add files via upload

--- aiw.py
+++ aiw.py
@@ -8,61 +8,93 @@ import seaborn as sns
 df = pd.read_csv('stardataset.csv')
 #ogólne dane statystyczne dot zmiennych (średnie, centyle itd)
 print(df.describe())
+#df.describe().to_csv("ogólne statystyki.csv")
 
-#temperatura vs. wielkość
-fig = plt.figure()
-ax = fig.add_subplot(111)
-brawn_dwarfs = df.loc[df['Star type'] == 0]
-x0 = brawn_dwarfs["Temperature (K)"]
-y0 = brawn_dwarfs["Absolute magnitude(Mv)"]
-red_dwarfs = df.loc[df['Star type'] == 1]
-x1 = red_dwarfs["Temperature (K)"]
-y1 = red_dwarfs["Absolute magnitude(Mv)"]
-white_dwarfs = df.loc[df['Star type'] == 2]
-x2 = white_dwarfs["Temperature (K)"]
-y2 = white_dwarfs["Absolute magnitude(Mv)"]
-main_sequences = df.loc[df['Star type'] == 3]
-x3 = main_sequences["Temperature (K)"]
-y3 = main_sequences["Absolute magnitude(Mv)"]
-supergiants = df.loc[df['Star type'] == 4]
-x4 = supergiants["Temperature (K)"]
-y4 = supergiants["Absolute magnitude(Mv)"]
-hypergiants = df.loc[df['Star type'] == 5]
-x5 = hypergiants["Temperature (K)"]
-y5 = hypergiants["Absolute magnitude(Mv)"]
 
-ax.scatter(x0, y0, c = 'orange', label = 'Brawn Dwarfs')
-ax.scatter(x1, y1, c = 'green', label = 'Red Dwarfs')
-ax.scatter(x2, y2, c = 'pink', label = 'White Dwarfs')
-ax.scatter(x3, y3, c = 'purple', label = 'Main Sequences')
-ax.scatter(x4, y4, c = 'grey', label = 'Supergiants')
-ax.scatter(x5, y5, c = 'yellow', label = 'Hypergiants')
 
+def wykres_wyodrebnione_typy_gwiazd(zmienna1, zmienna2):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    brawn_dwarfs = df.loc[df['Star type'] == 0]
+    x0 = brawn_dwarfs[zmienna1]
+    y0 = brawn_dwarfs[zmienna2]
+    red_dwarfs = df.loc[df['Star type'] == 1]
+    x1 = red_dwarfs[zmienna1]
+    y1 = red_dwarfs[zmienna2]
+    white_dwarfs = df.loc[df['Star type'] == 2]
+    x2 = white_dwarfs[zmienna1]
+    y2 = white_dwarfs[zmienna2]
+    main_sequences = df.loc[df['Star type'] == 3]
+    x3 = main_sequences[zmienna1]
+    y3 = main_sequences[zmienna2]
+    supergiants = df.loc[df['Star type'] == 4]
+    x4 = supergiants[zmienna1]
+    y4 = supergiants[zmienna2]
+    hypergiants = df.loc[df['Star type'] == 5]
+    x5 = hypergiants[zmienna1]
+    y5 = hypergiants[zmienna2]
+
+    ax.scatter(x0, y0, c = 'orange', label = 'Brawn Dwarfs')
+    ax.scatter(x1, y1, c = 'green', label = 'Red Dwarfs')
+    ax.scatter(x2, y2, c = 'pink', label = 'White Dwarfs')
+    ax.scatter(x3, y3, c = 'purple', label = 'Main Sequences')
+    ax.scatter(x4, y4, c = 'grey', label = 'Supergiants')
+    ax.scatter(x5, y5, c = 'yellow', label = 'Hypergiants')
+
+#temperatura vs. jasność
+wykres_wyodrebnione_typy_gwiazd("Temperature (K)", "Luminosity(L/Lo)")
 plt.legend()
-plt.ylabel("Temperature")
-plt.xlabel("Magnitude")
-plt.title("H-R Diagram of Total Stars ")
+plt.xlabel("Temperature")
+plt.ylabel("Luminosity")
+plt.show()
+
+#jasność vs. jasność względna
+wykres_wyodrebnione_typy_gwiazd("Luminosity(L/Lo)", "Absolute magnitude(Mv)")
+plt.legend()
+plt.xlabel("Luminosity")
+plt.ylabel("Magnitude")
+plt.show()
+
+#temperatura vs. jasność względna
+wykres_wyodrebnione_typy_gwiazd("Temperature (K)", "Absolute magnitude(Mv)")
+plt.legend()
+plt.xlabel("Temperature")
+plt.ylabel("Magnitude")
+plt.show()
+
+#radius vs. kuminosity
+x = df["Radius(R/Ro)"]
+y = df["Luminosity(L/Lo)"]
+plt.xlabel("Radius")
+plt.ylabel("Luminosity")
+plt.scatter(x, y, color = 'green')
+plt.show()
+####
+wykres_wyodrebnione_typy_gwiazd("Radius(R/Ro)", "Luminosity(L/Lo)")
+plt.legend()
+plt.xlabel("Temperature")
+plt.ylabel("Radius")
 plt.show()
 
 #temperatura vs. typ gwiazd
-y = df["Temperature (K)"]
 x = df["Star type"]
+y = df["Temperature (K)"]
 plt.xlabel("Star type")
 plt.ylabel("Temperature")
 plt.scatter(x, y, color = 'grey')
 plt.show()
 
-#jaką wielkość przyjmuje dany typ gwiazd
-y = df["Absolute magnitude(Mv)"]
+#jaką jasność względną przyjmuje dany typ gwiazd
 x = df["Star type"]
+y = df["Absolute magnitude(Mv)"]
 plt.xlabel("Star type")
 plt.ylabel("Magnitude")
 plt.scatter(x, y, color = 'orange')
 plt.show()
 
 #jaką jasność przyjmuje dany typ gwiazd
-y = df["Luminosity(L/Lo)"]
 x = df["Star type"]
+y = df["Luminosity(L/Lo)"]
 plt.xlabel("Star type")
 plt.ylabel("Luminosity")
 plt.scatter(x, y, color = 'orange')
